f79e9ae8ac98519c9bf721d11cf02b46afa3096c
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-04 14:26:44 +0100

    First test of using it with Java. Actually just a helloworld.

---
+++ extra_1/NumberPyramid.java
@@ -0,0 +1,13 @@
+package extra_1;
+
+public class NumberPyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
