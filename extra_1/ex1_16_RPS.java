b907a6ee666fb59b828a4784ec0cac08fac5a75e
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-04 17:00:54 +0100

    Program shell with comments of how to go about this program

---
+++ extra_1/ex1_16_RPS.java
@@ -0,0 +1,15 @@
+package extra_1;
+
+
+public class ex1_16_RPS {
+
+	//need to write a program to read in text form a file
+	// http://www.homeandlearn.co.uk/java/read_a_textfile_in_java.html looks useful for this
+	
+	public static void main(String[] args) {
+				
+
+
+	}
+
+}
