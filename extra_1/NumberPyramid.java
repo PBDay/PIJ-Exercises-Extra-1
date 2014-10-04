9cd34c49c3d77ff45e3e65cdca1aa9f0db4e482e
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-04 15:14:39 +0100

    Removes "Helllo world" Practice and adds program to output a number pyramid.

--- extra_1/NumberPyramid.java
+++ extra_1/NumberPyramid.java
@@ -1,12 +1,51 @@
 package extra_1;
+import java.util.Scanner;
 
 public class NumberPyramid {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int thisInt = 0;
+		Scanner kb = new Scanner(System.in);
 		
-		System.out.println("Hello World");
+		System.out.println("How high would you like the pyramid to go?");
+		
+		if (kb.hasNextInt())
+		{
+			thisInt=kb.nextInt();
+		}
+		else
+		{
+			System.out.println("Not a valid integer.");
+			NumberPyramid.main(args);
+		}
+		
+		kb.close();
+		
+		if (thisInt == -1)
+		{
+			System.out.println("Process cancelled");
+		}
+		else if (thisInt > 0) 
+		{
+			System.out.println("Number pyramid up to " + thisInt + ":");
+			
+			int currentInt=1;
+			
+			while (currentInt <= thisInt)
+			{
+				for (int i=1;i<=currentInt;i++)
+				{
+					System.out.print(currentInt);
+				}
+				System.out.println(" ");
+				currentInt++;	
+			}
+			
+		}
+			
+		
 		
 	}
 
