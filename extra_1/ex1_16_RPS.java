7b811f11c6a771204a8badfafa83ca91dc490c5a
Paul Day <pday02@mail.bkk.ac.uk>
2014-10-05 22:04:17 +0100

    First version of rock paper shotgun program

--- extra_1/ex1_16_RPS.java
+++ extra_1/ex1_16_RPS.java
@@ -1,5 +1,8 @@
 package extra_1;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class ex1_16_RPS {
 
@@ -8,8 +11,122 @@ public class ex1_16_RPS {
 	
 	public static void main(String[] args) {
 				
+		//firstly, check if input is a filename or a direct input
+		boolean inputFile = false;
+		
+		String file = "/Users/Stumpy/onedrive/documents/uniwork/pij/codes/test/extra_1/extra_1/ex1_16_RPS.txt";
+		
+		try 
+		{
+			String[] myText = functions.openFile(file);
+			inputFile=true;
+			int score =0; //positive for player 1, negative for player2 
+			int c=1;
+			boolean formatIssue = false;
+			int numGames = Math.floorDiv(myText[0].length() - 2, 4) + 1;
+		
+			while (!formatIssue && c<= numGames && Math.abs(score) <= 3)
+			{
+				if (c != 1 && !myText[0].substring( (c-1) * 4 - 2 ,(c-1) * 4).equals(", "))
+				{
 
+				formatIssue=true;	
+				}
+				else
+				{
+					int outcome = RPSResult(myText[0].substring(4 * (c - 1), 4 * (c - 1) + 2));				
+					if (outcome == -9)
+					{
+						formatIssue=true;
+					}
+					else
+					{
+						score += outcome;
+					}
+					
+				}
+				c++;
+			}
 
+			if (formatIssue)
+			{
+				System.out.println("File Format not correct.");
+			}
+			else if (score < -3)
+			{
+				System.out.println("Player 2 wins");
+			}
+			else if (score > 3)
+			{
+				System.out.println("Player 1 wins");
+			}
+			else
+			{
+				System.out.println("Neither player has won sufficiently.");
+			}
+
+		}
+		catch (IOException e)
+		{
+			System.out.println ("File not found, please enter results by keyboard:");
+			Scanner kb = new Scanner(System.in);
+			boolean exit = false;
+			int score = 0;
+			
+			while (!exit && Math.abs(score) <= 3)
+			{
+				System.out.println("Score is currently " + score + ". Please enter the next result or -1 to quit:");
+				String result = kb.next();
+				
+				if (result.equals("-1"))
+				{
+					exit = true;
+					System.out.println("Game quit. Score was " + score + ".");
+				}
+				else if (RPSResult(result) == -9)
+				{
+					System.out.println("Result not valid. No change made.");
+				}
+				else
+				{
+					score += RPSResult(result);
+				}
+			}
+			
+			if (score > 3)
+			{
+				System.out.println("Player 1 wins!");
+			}
+			else if (score < -3)
+			{
+				System.out.println("Player 2 wins!");
+			}
+			
+			
+		}
+		
+		
+	}
+	
+	private static int RPSResult(String actions)
+	{
+
+		if (actions.equals("RR") || actions.equals("PP") || actions.equals("SS") )
+		{
+			return 0;
+		}
+		else if (actions.equals("RS") || actions.equals("PR") || actions.equals("SP") )
+		{
+			return 1;
+		}
+		else if (actions.equals("RP") || actions.equals("PS") || actions.equals("SR") )
+		{
+			return -1;
+		}
+		else
+		{
+			return -9;
+		}
 	}
 
 }
